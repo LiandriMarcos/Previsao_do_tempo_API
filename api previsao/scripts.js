db10cc3152db616e443bf8492f50c976283c7851
Liandri <147012468+LiandriMarcos@users.noreply.github.com>
2024-12-06 22:22:42 -0300

    Add files via upload

---
+++ api previsao/scripts.js
@@ -0,0 +1,22 @@
+
+
+const key = "ce2b7777d91a6289de812f5f43a2ea64"
+
+function colocarDadosNaTela(dados) {
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+}
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+    buscarCidade(cidade)
+}
